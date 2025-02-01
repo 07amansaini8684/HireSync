26852841a806c685731ce763518bc75c3aff5f49
07amansaini8684 <mr.amannobody@gmail.com>
2025-02-02 01:34:33 +0530

    authentication fullyCompleted

--- convex/schema.ts
+++ convex/schema.ts
@@ -3,12 +3,11 @@ import { v } from "convex/values"
 
 export default defineSchema({
     users: defineTable({
-        user: v.string(),
+        name: v.string(),
         email: v.string(),
         image: v.optional(v.string()),
-        role: v.union(v.literal("candidate"), v.literal("interviewer")), // "candidate and interviewer"
-        clerkId: v.string(),  
-
-    }).index("by_clerk_id", ["clerkId"]),
+        role: v.union(v.literal("candidate"), v.literal("interviewer")),
+        clerkId: v.string(),
+      }).index("by_clerk_id", ["clerkId"]),
 })
 
